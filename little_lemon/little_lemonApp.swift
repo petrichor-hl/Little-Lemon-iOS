f7fc8fce6bee0f60f228f05ade232767f0bb8498
petrichor-hl <tl.hoanglam174@gmail.com>
2023-03-09 08:00:13 +0700

    Initial Commit

---
+++ little_lemon/little_lemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_lemonApp.swift
+//  little_lemon
+//
+//  Created by Trần Lê Hoàng Lâm on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct little_lemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
